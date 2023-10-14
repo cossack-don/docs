0bfd7f07a34b4490a368614addc4112febe8d858
cossack-done <ser90593696.zhuravlev@yandex.ru>
2023-10-14 20:57:06 +0300

    build: test

--- release.config.js
+++ release.config.js
@@ -1,21 +1,15 @@
-module.exports = {
-    branches: ["main"],
+const config = {
+    branches: ['main'],
     plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        [
-            "@semantic-release/github",
-            {
-                githubUrl: "https://github.com/cossack-don/docs",
-            },
-        ],
-        "@semantic-release/npm",
-        [
-            "@semantic-release/git",
-            {
-                assets: ["package.json"],
-                message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-            },
-        ],
-    ],
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/npm',
+        ['@semantic-release/git', {
+            'assets': ['package.json'],
+            'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        }],
+        '@semantic-release/github'
+    ]
 };
+
+module.exports = config;
