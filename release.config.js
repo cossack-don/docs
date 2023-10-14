4bacb80a3c4ad1fbba0077bc98dc7868d5336eb3
cossack-done <ser90593696.zhuravlev@yandex.ru>
2023-10-14 20:47:34 +0300

    feat: test

---
+++ release.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    branches: ["main"],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/github",
+            {
+                githubUrl: "https://github.com/cossack-don/docs",
+            },
+        ],
+        "@semantic-release/npm",
+        [
+            "@semantic-release/git",
+            {
+                assets: ["package.json"],
+                message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+            },
+        ],
+    ],
+};
