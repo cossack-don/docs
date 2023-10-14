c6986467cf1d50d5dc292b73f949bddfb1f4c0b6
cossack-done <ser90593696.zhuravlev@yandex.ru>
2023-10-14 23:59:16 +0300

    fix: test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {extends: ['@commitlint/config-conventional'],
 
-
+    // https://github.com/conventional-changelog/commitlint/issues/2112
     rules: {
         'body-max-line-length': [0, 'always'] // Make sure there is never a max-line-length by disabling the rule
     },
