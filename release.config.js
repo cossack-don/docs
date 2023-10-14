159b011e3225c07f46e90cc7e0fb47ed5514582c
cossack-done <ser90593696.zhuravlev@yandex.ru>
2023-10-14 22:29:55 +0300

    ci: test

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ const config = {
         '@semantic-release/npm',
         ['@semantic-release/git', {
             'assets': ['package.json'],
-            'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+            'message': 'chore(release): ${nextRelease.version}'
         }],
         '@semantic-release/github'
     ]
