7b84ced78402ad043a6358d946fa9d92b9e539b7
cossack-done <ser90593696.zhuravlev@yandex.ru>
2023-10-14 23:20:34 +0300

    feat: a

--- release.config.js
+++ release.config.js
@@ -1,15 +1,44 @@
-const config = {
-    branches: ['master', 'main'],
+// const config = {
+//     branches: ['master', 'main'],
+//     plugins: [
+//         '@semantic-release/commit-analyzer',
+//         '@semantic-release/release-notes-generator',
+//         '@semantic-release/npm',
+//         ['@semantic-release/git', {
+//             'assets': ['package.json'],
+//             'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+//         }],
+//         '@semantic-release/github'
+//     ]
+// };
+//
+// module.exports = config;
+
+module.exports = {
+    branches: [
+        'master',
+        {
+            name: 'beta',
+            prerelease: true
+        }
+    ],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: 'CHANGELOG.md'
+            }
+        ],
         '@semantic-release/npm',
-        ['@semantic-release/git', {
-            'assets': ['package.json'],
-            'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-        }],
-        '@semantic-release/github'
+        '@semantic-release/github',
+        [
+            '@semantic-release/git',
+            {
+                assets: ['CHANGELOG.md', 'dist/**'],
+                message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+            }
+        ]
     ]
-};
-
-module.exports = config;
+}
