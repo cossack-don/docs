c755b1595c65e0a39c19b8a8ef1f7de2ef3ece35
cossack-done <ser90593696.zhuravlev@yandex.ru>
2023-10-15 14:10:40 +0300

    next: fff

--- release.config.js
+++ release.config.js
@@ -1,7 +1,11 @@
 const config = {
     branches: ['master'],
     plugins: [
-        '@semantic-release/commit-analyzer',
+        '@semantic-release/commit-analyzer',{
+            "releaseRules": [
+                { "type": "next", "release": "major" },
+            ],
+        },
         '@semantic-release/release-notes-generator',
         '@semantic-release/npm',
         ['@semantic-release/git', {
