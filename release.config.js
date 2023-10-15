5ec5857ec92a4c5ff2551dfa8846ac1ef547017b
cossack-done <ser90593696.zhuravlev@yandex.ru>
2023-10-15 13:40:47 +0300

    breaking: 1

--- release.config.js
+++ release.config.js
@@ -1,8 +1,27 @@
 const config = {
     branches: ['master'],
     plugins: [
-        '@semantic-release/commit-analyzer',
+        '@semantic-release/commit-analyzer',{
+            "releaseRules": [
+                { "tag": "breaking", "release": "major" },
+                { "tag": "Build", "release": "minor" },
+                { "tag": "Chore", "release": "minor" },
+                { "tag": "Fix", "release": "patch" },
+                { "tag": "New", "release": "minor" },
+                { "tag": "Update", "release": "minor" },
+                { "tag": "Upgrade", "release": "minor" }
+            ]
+        },
         '@semantic-release/release-notes-generator',
+        {"releaseRules": [
+                { "tag": "breaking", "release": "major" },
+                { "tag": "Build", "release": "minor" },
+                { "tag": "Chore", "release": "minor" },
+                { "tag": "Fix", "release": "patch" },
+                { "tag": "New", "release": "minor" },
+                { "tag": "Update", "release": "minor" },
+                { "tag": "Upgrade", "release": "minor" }
+            ]},
         '@semantic-release/npm',
         ['@semantic-release/git', {
             'assets': ['package.json'],
