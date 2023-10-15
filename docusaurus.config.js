e8452132e91f8444fb18fa623c446152e756a6ce
cossack-done <ser90593696.zhuravlev@yandex.ru>
2023-10-15 21:09:23 +0300

    feat: added PWA

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,6 +14,34 @@ const config = {
 
   plugins: [
     [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          'appInstalled',
+          'standalone',
+          'queryString',
+        ],
+        pwaHead: [
+          {
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/docusaurus.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'manifest',
+            href: '/manifest.json', // your PWA manifest
+          },
+          {
+            tagName: 'meta',
+            name: 'theme-color',
+            content: 'rgb(37, 194, 160)',
+          },
+        ],
+      },
+    ],
+    [
       require.resolve("@cmfcmf/docusaurus-search-local"),
       {
         language: "ru",
