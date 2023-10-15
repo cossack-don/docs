ebcf2c583849e355a7151f0aab5cb9483bb21b03
cossack-done <ser90593696.zhuravlev@yandex.ru>
2023-10-15 14:28:15 +0300

    fix: change realise config

--- release.config.js
+++ release.config.js
@@ -2,23 +2,11 @@ const config = {
     branches: ['master'],
     plugins: [
         '@semantic-release/commit-analyzer',{
-            "preset": "angular",
-            "releaseRules": [
-                { "type": "breaking", "release": "major" },
-            ],
         },
         '@semantic-release/release-notes-generator',{
-            "preset": "angular",
-            "releaseRules": [
-                { "type": "breaking", "release": "major" },
-            ],
         },
         '@semantic-release/npm',
         ['@semantic-release/git', {
-            "preset": "angular",
-            "releaseRules": [
-                { "type": "breaking", "release": "major" },
-            ],
             'assets': ['package.json'],
             'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
         }],
